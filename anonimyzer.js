4663c809bbc6f70aad26c2b69ae7d9df53f7d070
ussaohelcim <57050328+ussaohelcim@users.noreply.github.com>
2021-05-27 18:13:04 -0300

    updated comments

--- anonimyzer.js
+++ anonimyzer.js
@@ -1,60 +1,85 @@
 const conf = require("./config.json")
-
-
 const Discord = require('discord.js');
-const robo = new Discord.Client();
+const bot = new Discord.Client();
 
-robo.login(conf.TOKEN);
+bot.login(conf.TOKEN);
 
 password = getRandomInt(1,30);
 
-messages = []
-
-robo.on('ready', ()=>{//i dont know what is this, but this its called when the bot starts
+bot.on('ready', ()=>{
+    //event called every time that bot is ready
     console.log("bot started");
-    robo.user.setActivity("lulz pra todo lado");
+    bot.user.setActivity("lulz pra todo lado");//"Playing "
 })
-robo.on('message', message =>{//i dont know what is this, but this its called when someone sends a message
+bot.on('message', message =>{
+/*event called every time that someone/something sends 
+message to some server that the bot is */
+
 
     if(message.channel.id === conf.TOSENDCHANNEL)
-    {//check if the message its on "toSendChannel"
-        console.log(message.author.tag," said " ,message.content);//display in the console the message author and the message
-        msg = encriptId(message.author.id)+" >: ```"+ message.content+"```";
-        console.log(encriptId(message.author.id))
+    {//check if the actual message its on "toSendChannel"
+
+        console.log(message.author.tag," said " ,message.content);
+        //display in the console the message author and the message content
+
+        msg = encryptId(message.author.id)+" >: ```"+ message.content+"```";
+        //encrypt the id of the message author and put inside a code quote
+
+        //console.log(encriptId(message.author.id))
         message.delete();//delete the message
-        robo.channels.cache.get(conf.TOREADCHANNEL).send(msg);//send the message to the "toReadChannel"
-        messages.unshift(msg)
+
+        bot.channels.cache.get(conf.TOREADCHANNEL).send(msg);
+        //send the message to the "toReadChannel"
+
     }
 })
-robo.on('guildMemberAdd',member =>{
-    message.send("testando ",member);
+bot.on('guildMemberAdd',member =>{
+    anonNickname(member)
+    //bot.channels.cache.get(conf.TOREADCHANNEL).send();
 })
-robo.on('rateLimit',a =>{
+bot.on('rateLimit',a =>{
+    console.log("Rate limit reached.")
     console.log(a)
 })
 
-function getRandomInt(min, max) {
+function getRandomInt(min, max) 
+{//returns a pseudo-random number between "min" and "max"
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function encriptId(id)
+function encryptId(id)
 {
     //32,255
 
-    randomcharss = ["0","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","_","`","{","|","}","~","D","w","x","y","z","A","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","1","8","9","a","b","c","d","e","f","g","2","#","$","%","&","'","(",")","*","+",",","-","B","C","Y","Z","!","G","H","I","J",".","/",":",";","<","=",">","?","@","[","^","E","F","3","4","5","6","7",]
+    randomcharss = ["0","h","i","j","k","l","m","n","o","p","q","r","s","t","u",
+    "v","_","`","{","|","}","~","D","w","x","y","z","A","K","L","M","N","O","P",
+    "Q","R","S","T","U","V","W","X","1","8","9","a","b","c","d","e","f","g","2",
+    "#","$","%","&","'","(",")","*","+",",","-","B","C","Y","Z","!","G","H","I",
+    "J",".","/",":",";","<","=",">","?","@","[","^","E","F","3","4","5","6","7",]
     arr = "";
     for (let index = 0; index < id.length; index++) {
         var txt = id[index];
         //console.log(txt)
         arr+=randomcharss[parseInt(txt,10)+password]
     }
-
     return arr;
 }
-let teste = process.openStdin()
-teste.addListener('data',function(a){
-    let t = a.toString()
-    robo.channels.cache.get(conf.TOREADCHANNEL).send(t);
+function anonNickname(who)
+{//set a new nickname for "who", with 24 pseudo-random numbers
+    let nick = "";
+    for (let index = 0; index <= 24; index++) {
+        nick+= getRandomInt(0,9)
+    }
+    who.setNickname(nick)
+}
+
+
+let c = process.openStdin();//reads what you write at stdin
+
+c.addListener('data',function(a){
+    let t = a.toString();//the string that you writed
+    bot.channels.cache.get(conf.TOREADCHANNEL).send(t);
+    //send what you writed to the "to read channel"
 })
